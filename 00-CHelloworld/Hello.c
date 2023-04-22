684c8dc99f4dcbe8be380883cdd0550b5550cb5b
Jose Francisco Terraza <jterraza@frba.utn.edu.ar>
2023-04-21 21:01:32 -0300

    version final 3.01

--- 00-CHelloworld/Hello.c
+++ 00-CHelloworld/Hello.c
@@ -1,8 +1,20 @@
 #include <stdio.h> 
 #include <stdlib.h>
 
-int main(int argc, char const *argv[])
-{
-    printf("Hola Mundo!"); 
-    return 0;
+main() { 
+    FILE *archivo;
+    char caracter;
+    
+    archivo=fopen("output.txt", "r");
+    if(archivo==NULL){
+    	printf("Error en la apertura del archivo\n");
+	} else {
+		while(feof(archivo)==0){
+			caracter=fgetc(archivo);
+			printf("%c", caracter);
+		}
+	}
+	fclose(archivo);
+	getch();
+	return 0;
 }
